fb893dbed50e79c45ff0e5d5fbe9d4bcb34b6c0a
goodybestie <goodnessbassey003@gmail.com>
2024-10-14 06:16:54 -0700

    a new project

---
+++ product-preview/src/Data/index.jsx
@@ -0,0 +1,4 @@
+ export {default as cart} from "./images/icon-cart.svg"
+ export {default as favicon} from "./images/favicon.png"
+ export {default as product1} from "./images/product1.jpg"
+ export {default as product2} from "./images/product2.jpg"
